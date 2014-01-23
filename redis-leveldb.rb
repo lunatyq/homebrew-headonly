c4a3843610a4d48d95f0c10c934f0808500f0e7a
lunatyq <maciej@lslabs.eu>
2014-01-23 13:14:45 +0100

    Updated url and skipped submodule init/update - not needed

--- redis-leveldb.rb
+++ redis-leveldb.rb
@@ -1,13 +1,11 @@
 require 'formula'
 class RedisLeveldb < Formula
-  head 'https://github.com/KDr2/redis-leveldb'
+  head 'git://git@github.com:KDr2/redis-leveldb'
 
   depends_on 'libev'
   depends_on 'gmp'
 
   def install
-    system("git submodule init")
-    system("git submodule update")
     system("make")
     bin.install("redis-leveldb")
   end
