e788945d4e1e1db1a8bbc6ba1bbbdbf4adc3f7e8
lunatyq <maciej@lslabs.eu>
2014-01-23 12:58:18 +0100

    Redis-LevelDb draft

---
+++ redis-leveldb.rb
@@ -0,0 +1,13 @@
+class RedisLeveldb < Formula
+  head 'https://github.com/KDr2/redis-leveldb'
+
+  depends_on :libev
+  depends_on :gmp
+
+  def install
+    system("git submodule init")
+    system("git submodule update")
+    system("make")
+    bin.install("redis-leveldb")
+  end
+end
