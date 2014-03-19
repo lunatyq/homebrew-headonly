6088c5749aaedc4372170f92f03a874ca659009d
lunatyq <maciej@lslabs.eu>
2014-03-19 13:28:40 +0100

    Redis-leveldb style fix

--- redis-leveldb.rb
+++ redis-leveldb.rb
@@ -1,12 +1,13 @@
 require 'formula'
+
 class RedisLeveldb < Formula
-  head 'git://github.com/KDr2/redis-leveldb'
+  head 'https://github.com/KDr2/redis-leveldb'
 
   depends_on 'libev'
   depends_on 'gmp'
 
   def install
-    system("make")
-    bin.install("redis-leveldb")
+    system 'make'
+    bin.install 'redis-leveldb'
   end
 end
